f2aa47e6acaac297a6f02824a6ff3f256656a34f
Josiel Lopez <jdlopez114@gmail.com>
2022-03-22 20:44:27 -0400

    commit tree 1c1ac31ad53b63d00a253a6b095399efa97dc03f

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector('#text').textContent = "This is really cool!";
+  });
+  
+
+  // function that runs once DOM is loaded
+
+//   const replaceText = function(){
+//       document.querySelector('#text').textContent = "This is really cool!"
+//   };
